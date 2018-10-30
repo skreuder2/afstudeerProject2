3442fbf38752e7665993fa053c657e4bd325853c
skreuder2 <44606803+skreuder2@users.noreply.github.com>
2018-10-30 12:51:17 +0100

    Update my_script.js

--- script/my_script.js
+++ script/my_script.js
@@ -7,18 +7,18 @@
 //     {img:"/img/2.gif", name: "Aarde"}
 // )
 
-var ads = new Array(
-    {img:"img/animated/apples_anim.gif", name: "Amsterdam Apples",
-    profile:"img/profile_img/apples.jpg", desc: "Onze appels bevatten van nature weinig caloriën en zitten bovendien boordevol vezels, wat zorgt voor een voldaan gevoel. Het perfecte tussendoortje!"},
-    {img:"img/animated/coffee_anim.gif", name: "Het KoffieBedrijf",
-    profile:"img/profile_img/coffee.jpg", desc: "Hee student! Toe aan een break? Kom langs bij een van onze vestigingen voor wat vloeibare concentratie."},
-    {img:"img/animated/schoonmaak_anim.gif", name: "Huur een Hulp",
-    profile:"img/profile_img/schoonmaak.png", desc: "Feestje gegeven, komen je ouders over de vloer of gewoon toe aan een geheel fris huis? Naast de wekelijkse schoonmaakdienst bieden wij vanaf nu ook een eenmalige 'Grote schoonmaak' aan!"},
-    {img:"img/animated/tanden_anim.gif", name: "Tanden Witten Thuis",
-    profile:"img/profile_img/tanden.jpg", desc: "Tanden witten thuis is de webshop om makkelijk en voordelig uw tanden thuis te witten. Je Tanden Witten Thuis is nog nooit zo makkelijk geweest"},
-    {img:"img/animated/water_anim.gif", name: "Botteled.",
-    profile:"img/profile_img/water.jpg", desc: "Gemiddeld moet een mens 1,5 tot 2 liter water per dag drinken om goed te functioneren. Met de nieuwe 2L Bottles. is het nog makkelijker om gehydrateerd te blijven!"}
-)
+// var ads = new Array(
+//     {img:"img/animated/apples_anim.gif", name: "Amsterdam Apples",
+//     profile:"img/profile_img/apples.jpg", desc: "Onze appels bevatten van nature weinig caloriën en zitten bovendien boordevol vezels, wat zorgt voor een voldaan gevoel. Het perfecte tussendoortje!"},
+//     {img:"img/animated/coffee_anim.gif", name: "Het KoffieBedrijf",
+//     profile:"img/profile_img/coffee.jpg", desc: "Hee student! Toe aan een break? Kom langs bij een van onze vestigingen voor wat vloeibare concentratie."},
+//     {img:"img/animated/schoonmaak_anim.gif", name: "Huur een Hulp",
+//     profile:"img/profile_img/schoonmaak.png", desc: "Feestje gegeven, komen je ouders over de vloer of gewoon toe aan een geheel fris huis? Naast de wekelijkse schoonmaakdienst bieden wij vanaf nu ook een eenmalige 'Grote schoonmaak' aan!"},
+//     {img:"img/animated/tanden_anim.gif", name: "Tanden Witten Thuis",
+//     profile:"img/profile_img/tanden.jpg", desc: "Tanden witten thuis is de webshop om makkelijk en voordelig uw tanden thuis te witten. Je Tanden Witten Thuis is nog nooit zo makkelijk geweest"},
+//     {img:"img/animated/water_anim.gif", name: "Botteled.",
+//     profile:"img/profile_img/water.jpg", desc: "Gemiddeld moet een mens 1,5 tot 2 liter water per dag drinken om goed te functioneren. Met de nieuwe 2L Bottles. is het nog makkelijker om gehydrateerd te blijven!"}
+// )
 
 var adsStatic = new Array(
     {img:"img/static/apples_static.jpg", name: "Amsterdam Apples",
